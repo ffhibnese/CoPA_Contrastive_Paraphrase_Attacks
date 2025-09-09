b073dfd7ce84cefd961d4e31dd2d1ab4389132cc
FFHow <91327274+ffhibnese@users.noreply.github.com>
2025-09-09 11:54:21 +0800

    Update paraphrase.sh

--- paraphrase.sh
+++ paraphrase.sh
@@ -7,5 +7,5 @@ M=qwen2.5-72b
 file_name="${D}_${source_model}.raw_data.json"
 
 CUDA_VISIBLE_DEVICES=0,1,2 python scripts/data_builder.py --data_name $D --dataset $data_path/$file_name \
-        --n_samples 150 --lamda 0.5 --paraphrase --do_temperature  --temperature 1 --max_tries 10 \
+        --n_samples 150 --lamda 0.5 --paraphrase --do_temperature  --temperature 1 --max_tries 5 \
         --batch_size 1 --alpha 1e-5 --base_model_name $M --output_file $data_path/${D}_${M}_${source_model}
